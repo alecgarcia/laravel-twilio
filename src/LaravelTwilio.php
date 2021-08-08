0323a8ce83eb3a2d0b37039eb8d5012db46354d3
Alec Garcia <alec@hyrograsper.com>
2021-08-07 20:29:28 -0500

    Add basic structure of Twilio class

--- src/LaravelTwilio.php
+++ src/LaravelTwilio.php
@@ -2,7 +2,39 @@
 
 namespace Alecgarcia\LaravelTwilio;
 
+use Twilio\Exceptions\ConfigurationException;
+use Twilio\Exceptions\TwilioException;
+use Twilio\Rest\Api\V2010\Account\CallInstance;
+use Twilio\Rest\Api\V2010\Account\MessageInstance;
+use Twilio\Rest\Client;
+use Twilio\TwiML\TwiML;
+use Twilio\TwiML\VoiceResponse;
+
 class LaravelTwilio
 {
-    // Build your next great package.
+    protected $sid;
+    protected $token;
+    protected $verifySSL;
+    protected $twilio;
+
+    public function __construct(string $sid, string $token, bool $verifySSL = true)
+    {
+        $this->sid = $sid;
+        $this->token = $token;
+        $this->verifySSL = $verifySSL;
+    }
+
+    public function getInstance()
+    {
+        if ($this->twilio) {
+            return $this->twilio;
+        }
+
+        try {
+            return $this->twilio = new Client($this->sid, $this->token);
+        } catch (ConfigurationException $configurationException) {
+            // TODO: Do something with the exception.
+        }
+    }
+
 }
